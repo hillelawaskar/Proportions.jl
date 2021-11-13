4ced7e7ce7734de877ae69887f6f0d2da7203638
Hillel <hillel.awaskar@gmail.com>
2021-11-13 21:41:00 +0530

    Test completed with 2d

--- src/twodproportion.jl
+++ src/twodproportion.jl
@@ -5,7 +5,7 @@ returns a AbstractArray{Float64, 2}
 # Example
 ```
 julia> get_proportion_2d([1.0 2.0 ; 4.0 5.0])
-[0.08333333333333333 0.16666666666666666; 0.3333333333333333 0.4166666666666667]
+=[0.08333333333333333 0.16666666666666666; 0.3333333333333333 0.4166666666666667]
 ```
 """
 function get_proportion_2d(for_prop_arr::AbstractArray{Float64,2})::AbstractArray{Float64,2}
@@ -14,43 +14,40 @@ end
 
 """
     get_proportion_round_2d(for_prop_arr::AbstractArray{Float64, 2};round_digits::Int64)::AbstractArray{Float64, 2}
-Compute the proportional values for each element in the array and round to a specified decimal digits.
+Compute the proportional values for each element in the 2d array and round to a specified decimal digits.
 If the sum of all the elements is not 1 , then the adjustment is done in the 1st largest element
 returns a AbstractArray{Float64, 1}
 # Example
 ```
-julia> get_proportion_round_2d([1.33,1.33,1.23,1.1111],round_digits = 2)
-=[0.26, 0.27, 0.25, 0.22]
+julia> get_proportion_round_2d([1.1101 2.243 ; 4.9898 5.87],round_digits=5)
+=[0.07811 0.15781; 0.35108 0.413]
 ```
 """
 function get_proportion_round_2d(for_prop_arr::AbstractArray{Float64,2};round_digits::Int64 = 2)::AbstractArray{Float64,2}
     temparr=  round.(for_prop_arr ./ sum(for_prop_arr), digits=round_digits, base = 10)
     diff = 1 - sum(temparr) #delta naming
     if diff != 0.0
-        println(diff)
         max = argmax(temparr)
-        print(max)
         temparr[max] =  round(temparr[max] + diff, digits=round_digits, base = 10)
     end
     return temparr
 end
 
 """
-    get_proportion_round_add1251(for_prop_arr::Array{Float64, 1};round_digits::Int64)::Array{Float64, 1}
-Compute the proportional values for each element in the array and round to a specified decimal digits.
+    get_proportion_round_add1251(for_prop_arr::AbstractArray{Float64, 2};round_digits::Int64)::AbstractArray{Float64, 2}
+Compute the proportional values for each element in the 2d array and round to a specified decimal digits.
 If the sum of all the elements is not 1 , then add 1251 to the array and try looking for proportional correctness
 after 2 itterations , the adjustment is done in the 1st largest element
-returns a Array{BigFloat, 1}
+returns a AbstractArray{Float64, 2}
 # Example
 ```
-julia> get_proportion_round_add1251([1.33,1.33,1.23,1.1111],round_digits = 2)
-=[0.26, 0.27, 0.25, 0.22]
+julia> get_proportion_round_add1251_2d([1.1101 2.243 ; 4.9898 5.87 ; 34.333 6.78],round_digits=8)
+=[0.16559399 0.16574381; 0.16610708 0.16622349; 0.16998779 0.16634384]
 ```
 """
 function get_proportion_round_add1251_2d(for_prop_arr::AbstractArray{Float64,2};round_digits::Int64 = 2)::AbstractArray{Float64,2}
     temparr=  round.(for_prop_arr ./ sum(for_prop_arr), digits=round_digits, base = 10)
     diff = 1 - sum(temparr)
-    println(diff)
     if diff != 0.0
         check = 1
         while check < 2
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     @test Proportions.get_proportion([1.0,1.0,1.0,2.5]) != [0.25,0.25,0.25,0.25]
     @test Proportions.get_proportion_round([1.33,1.33,1.23,1.1111],round_digits=2) ==[0.26, 0.27, 0.25, 0.22]
     @test Proportions.get_proportion_round([33,33,33.23,33.1111],round_digits=6) ==[0.249356, 0.249356, 0.251093, 0.250195]
-    @test Proportions.get_proportion_round_add1251([33.009, 33.31223, 33.2113, 33.111122],round_digits=6) ==[0.24997, 0.250029, 0.25001, 0.24999]
+    @test Proportions.get_proportion_round_add1251([33.009, 33.31223, 33.2113, 33.111122],round_digits=6) ==[0.24997, 0.25003, 0.25001, 0.24999]
     @test Proportions.get_proportion_2d([1.0 2.0 ; 4.0 5.0]) == [0.08333333333333333 0.16666666666666666; 0.3333333333333333 0.4166666666666667]
     @test Proportions.get_proportion_round_2d([1.1101 2.243 ; 4.9898 5.87],round_digits=5) == [0.07811 0.15781; 0.35108 0.413]
     @test Proportions.get_proportion_round_add1251_2d([1.1101 2.243 ; 4.9898 5.87 ; 34.333 6.78],round_digits=8) == [0.16559399 0.16574381; 0.16610708 0.16622349; 0.16998779 0.16634384]
